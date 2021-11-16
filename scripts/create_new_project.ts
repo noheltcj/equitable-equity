30b61ea4784fb2ff7a75de54db99612a916daf45
Colton Nohelty <noheltycolton@gmail.com>
2021-11-16 04:12:28 -0600

    Format

--- scripts/create_new_project.ts
+++ scripts/create_new_project.ts
@@ -1,21 +1,32 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const EquitableEquityDAO = await ethers.getContractFactory("EquitableEquityDAO");
+  const EquitableEquityDAO = await ethers.getContractFactory(
+    "EquitableEquityDAO"
+  );
 
   /** Will need to be updated with the address of any newly deployed DAOs. */
-  const dao = EquitableEquityDAO.attach("0x0165878A594ca255338adfa4d48449f69242Eb8F");
+  const dao = EquitableEquityDAO.attach(
+    "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+  );
 
   const createdProjectTokenAddress = await dao.createProject(
     "TestProject",
     "TST",
     dao.address,
     100
-  )
+  );
 
-  const fetchedProjectTokenAddress = await dao.getAddressOfProjectToken("TestProject")
+  const fetchedProjectTokenAddress = await dao.getAddressOfProjectToken(
+    "TestProject"
+  );
 
-  console.log("created:", createdProjectTokenAddress, "\nfetched:", fetchedProjectTokenAddress)
+  console.log(
+    "created:",
+    createdProjectTokenAddress,
+    "\nfetched:",
+    fetchedProjectTokenAddress
+  );
 }
 
 main().catch((error) => {
