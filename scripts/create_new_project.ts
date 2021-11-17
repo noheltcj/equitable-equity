3ceb93291ec165d11f7fa969ac6a8b952a28eb9a
Colton Nohelty <noheltycolton@gmail.com>
2021-11-17 02:11:05 -0600

    Interaction is a work in progress

--- scripts/create_new_project.ts
+++ scripts/create_new_project.ts
@@ -5,21 +5,29 @@ async function main() {
     "EquitableEquityDAO"
   );
 
+  const EquitableEquityProjectDAO = await ethers.getContractFactory(
+    "EquitableEquityProjectDAO"
+  );
+
   /** Will need to be updated with the address of any newly deployed DAOs. */
   const dao = EquitableEquityDAO.attach(
-    "0x0165878A594ca255338adfa4d48449f69242Eb8F"
+    "0x5FbDB2315678afecb367f032d93F642f64180aa3"
   );
 
   const createdProjectDAOAddress = await dao.createProject(
-    "TestProject",
-    "TST",
-    dao.address,
+    "Test Project 5",
+    "TST7",
+    "0xaDE9673b775B34c0EC2101d1Ac60fBBBCaB3FBc7",
     100
   );
 
-  await createdProjectDAOAddress.wait();
+  const receipt = await createdProjectDAOAddress.wait()
 
-  console.log("created:", createdProjectDAOAddress.chainId);
+  const projects = await dao.listProjects()
+  console.log("Projects...", projects)
+  console.log("Using address...", receipt.contractAddress)
+  const projectDao = EquitableEquityProjectDAO.attach(receipt.contractAddress)
+  console.log("ORG NAME:", projectDao.organizationName);
 }
 
 main().catch((error) => {
